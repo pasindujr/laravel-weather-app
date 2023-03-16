076c2c96e482d9462eacc6352c840a380c867234
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-03-16 22:49:46 +0530

    Routes defined and controller created.

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\WeatherRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class WeatherController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('weather');
+    }
+
+    public function getWeather(WeatherRequest $request)
+    {
+
+        $city = $request->input('city');
+        $apiKey = config('weather.openweathermap_api_key');
+        $response = Http::get("https://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$apiKey}&units=metric");
+        $weatherData = $response->json();
+
+        return view('weather', compact('weatherData'));
+    }
+
 }
